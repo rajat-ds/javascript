a77c1542ae4c99b72b7e25837336def79c772376
rajat-ds <rajatkumarbind@gmail.com>
2021-01-06 00:08:21 +0530

    poylfill Of Bind

---
+++ concept/polyfillOfBind.js
@@ -0,0 +1,22 @@
+const name = {
+    firstName : 'Rajat',
+    lastName : 'Kashyap'
+  }
+  
+  let printName = function (hometown,homeState){
+  console.log(this.firstName +" "+ this.lastName+ " " + hometown+ " " + homeState);
+  }
+  
+  let printMyName = printName.bind(name,"Allahabad");
+  printMyName("Uttar Pradesh");
+  
+  Function.prototype.myBind = function (...args){
+      let obj = this;
+      let param = args.slice(1)
+      return function(...args2){
+          obj.apply(args[0],[...param,...args2]);
+      }
+      
+  }
+  let printMyName2 = printName.myBind(name,"Allahabad");
+  printMyName2("Uttar Pradesh");
