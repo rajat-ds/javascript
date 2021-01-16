f9a8c6bea67cea7fe16da2b76364fe915e4d092b
rajat-ds <rajatkumarbind@gmail.com>
2021-01-16 11:38:02 +0530

    typeof javascirpt

---
+++ concept/typeOF.js
@@ -0,0 +1,21 @@
+The typeof operator returns a string indicating the type of the unevaluated operand .
+
+console.log(typeof 42);
+// expected output: "number"
+
+console.log(typeof 'blubber');
+// expected output: "string"
+
+console.log(typeof true);
+// expected output: "boolean"
+
+console.log(typeof undeclaredVariable);
+// expected output: "undefined"
+
+typeof "John Doe" // Returns "string"
+typeof 3.14 // Returns "number"
+typeof true // Returns "boolean"
+typeof 234567890123456789012345678901234567890n // Returns bigint
+typeof undefined // Returns "undefined"
+typeof null // Returns "object" (kind of a bug in JavaScript)
+typeof Symbol('symbol') // Returns Symbol
