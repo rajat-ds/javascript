accd6029a6e252a568cbd72ad411d2154c92b4c5
rajat-ds <rajatkumarbind@gmail.com>
2021-03-10 01:01:42 +0530

    added diffent types of console

---
+++ concept/console.js
@@ -0,0 +1,19 @@
+console.time('Your code Took');
+console.log('Hello console');
+console.log(4+34);
+console.log(34);
+console.log(true);
+console.log([34,2,1,2]);
+console.log({harry: 'this', marks:34});
+console.table({harry: 'this', marks:34});
+console.warn('This is a warning');
+// console.clear();
+console.timeEnd('Your code Took');
+// console.assert(566<189, 'Age >189 is not possible')
+// console.error('This is an error')
+
+/*
+this
+is a
+multiline comment
+*/
