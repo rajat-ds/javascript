85b2f795690a45eabe74cc756932b31905b4a929
rajat-ds <rajatkumarbind@gmail.com>
2021-03-15 00:47:24 +0530

    added fetch api

---
+++ concept/fetch_api.js
@@ -0,0 +1,32 @@
+function getData(){
+    console.log("Started getData")
+    url = "https://api.github.com/users";
+    fetch(url).then((response)=>{
+        console.log("Inside first then")
+        return response.json();
+    }).then((data)=>{
+        console.log("Inside second then")
+        console.log(data);
+    })
+}
+
+
+function postData(){
+    url = "http://dummy.restapiexample.com/api/v1/create";
+    data = '{"name":"harglry347485945","salary":"123","age":"23"}'
+    params = {
+        method:'post',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: data
+    }
+    fetch(url, params).then(response=> response.json())
+    .then(data => console.log(data)
+    )
+}
+//body_always accepts string
+// console.log("Before running getData")
+// getData()
+// console.log("After running getData")
+postData()
