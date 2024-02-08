44bcfd2caad0abb63af9af137c18203161e4ccdc
Mr. Kashyap <rajatkumarbind@gmail.com>
2024-02-08 15:08:28 +0530

    polyfill of bind

--- concept/polyFills/polyfillOfBind.js
+++ concept/polyFills/polyfillOfBind.js
@@ -9,14 +9,15 @@ const name = {
   
   let printMyName = printName.bind(name,"Allahabad");
   printMyName("Uttar Pradesh");
-  
-  Function.prototype.myBind = function (...args){
-      let obj = this;
-      let param = args.slice(1)
-      return function(...args2){
-          obj.apply(args[0],[...param,...args2]);
-      }
-      
+
+
+  Function.prototype.myBind  =  function (...args){
+     let context = this
+     let param = args.slice(1)
+    return function(...args2){
+        context.apply(args[0] , [...param , ...args2] )
+    }
   }
+
   let printMyName2 = printName.myBind(name,"Allahabad");
   printMyName2("Uttar Pradesh");
