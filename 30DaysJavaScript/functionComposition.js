e68fe041f775ad6fcb95e02641d8d6731b6e894b
Mr. Kashyap <rajatkumarbind@gmail.com>
2024-02-12 13:15:04 +0530

    function composition

---
+++ 30DaysJavaScript/functionComposition.js
@@ -0,0 +1,19 @@
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var compose = function (functions) {
+  return function (x) {
+    // let ans = x
+    // for( let i = functions.length -1 ; i >= 0 ; i--){
+    //      ans = functions[i](ans)
+    // }
+    // return ans
+    return functions.reduceRight((acc, fn) => fn(acc), x);
+  };
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
